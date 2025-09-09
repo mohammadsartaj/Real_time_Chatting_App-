4be6370958e0ead95a9c16281d04011bdca1c136
Mohammad  Sartaj <sartajmohammad003@gmail.com>
2025-09-09 14:01:49 +0530

    first commit

---
+++ src/test/java/com/ecommers/ChatApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
